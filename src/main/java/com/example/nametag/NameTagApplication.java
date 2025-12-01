23b8e98491e46c644b903b2b6d81886049aaf587
cnb1 <hackerrank134@gmail.com>
2025-12-01 12:55:24 -0500

    feat: scaffold initial Spring Boot application with Maven, .gitignore, and basic structure.

---
+++ src/main/java/com/example/nametag/NameTagApplication.java
@@ -0,0 +1,13 @@
+package com.example.nametag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameTagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameTagApplication.class, args);
+	}
+
+}
